04d342102d3f7ea2410a64c7003510d5e6104f60
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-11-12 22:07:07 +0200

    Update headless.test.js

--- tests/headless.test.js
+++ tests/headless.test.js
@@ -3,33 +3,12 @@ import { ai } from '@zerostep/playwright';
 import fs from 'fs';
 import headless from 'headless';
 
-// let xvfbStarted = false;
-
-// async function startXvfb() {
-  return new Promise((resolve, reject) => {
-    headless((err, childProcess, servernum) => {
-      if (err) {
-        console.error('Error starting Xvfb:', err);
-        reject(err);
-      } else {
-        console.log('Xvfb running on server number', servernum);
-        xvfbStarted = true;
-        resolve();
-      }
-    });
-  });
-}
 
 test.describe('zerostep example', () => {
-  test.beforeAll(async () => {
-    if (!xvfbStarted) {
-      await startXvfb();
-    }
-  });
 
   test('captures screenshot after navigating to page', async ({ page }) => {
     const aiArgs = { page, test };
-    await page.goto('https://new.hollywoodbets.net/');
+    await page.goto('https://whatsmyuseragent.com/');
     await page.waitForLoadState('networkidle');
 
     const screenshotDir = '/app/results';
