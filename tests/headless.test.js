72f570ad11d1d9f96f8c71dc3a27908d1467d5ac
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-11-13 16:43:48 +0200

    Update headless.test.js

--- tests/headless.test.js
+++ tests/headless.test.js
@@ -10,16 +10,30 @@ test.describe('zerostep example', () => {
     const aiArgs = { page, test };
     await page.goto('https://new.hollywoodbets.net/');
     await page.waitForLoadState('networkidle');
+    
+    const htmlContent = await page.content();
+    const outputDir = '/app/results';
+    
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir, { recursive: true });
+    };
+    fs.writeFileSync(`${outputDir}/page_content.html`, htmlContent, 'utf-8');
+    console.log("HTML content saved to /app/results/page_content.html");
+    
+    await page.screenshot({ path: `${outputDir}/search_results1.png`, fullPage: true });
+    console.log("Screenshot taken and saved to /app/results/search_results1.png");
+                    
     await ai('Click on the checkbox (Verify you are human)', aiArgs).catch( e => {
       console.log("Failed to click checkbox", e.message);
     });
+    await page.waitForTimeOut(3000);
     await page.waitForLoadState('networkidle');
-    const screenshotDir = '/app/results';
-    if (!fs.existsSync(screenshotDir)) {
-      fs.mkdirSync(screenshotDir, { recursive: true });
-    }
+    // const screenshotDir = '/app/results';
+    // if (!fs.existsSync(screenshotDir)) {
+    //  fs.mkdirSync(screenshotDir, { recursive: true });
+    // }
 
-    await page.screenshot({ path: `${screenshotDir}/search_results.png`, fullPage: true });
-    console.log("Screenshot taken and saved to /app/results");
+    await page.screenshot({ path: `${outputDir}/search_results2.png`, fullPage: true });
+    console.log("Screenshot taken and saved to /app/results/search_results2.png");
   });
 });
